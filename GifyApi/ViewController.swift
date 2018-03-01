8fa78ae8a573f19f60f8e1cbeabe8763de37b523
Perfect <perfect.aduh@gmail.com>
2018-03-01 14:22:35 +0100

    Initial Commit

---
+++ GifyApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GifyApi
+//
+//  Created by Perfect Aduh on 01/03/2018.
+//  Copyright © 2018 Perfect Aduh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
